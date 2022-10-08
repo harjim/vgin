3e40d7c920caf2552d98e3494378de39109a31a5
harjim <zhouping.h.app@outlook.com>
2022-10-08 15:39:29 +0800

    build: 单元测试约束

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,30 @@
+/// <reference types="vitest" />
+
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import { defineConfig, loadEnv } from 'vite'
-import { useEnv } from './src/utils/useEnv'
+
+const useEnv = (env: Recordable<unknown>): ImportMetaEnv => {
+  const ret: unknown = {}
+  for (const envKey of Object.keys(env)) {
+    let envVal = env[envKey]
+
+    envVal = envVal === 'true' ? true : envVal === 'false' ? false : envVal
+
+    if (/^[1-9]+[0-9]*]*$/.test(envVal as string)) {
+      envVal = parseInt(envVal as string)
+    }
+
+    ret[envKey] = envVal
+  }
+  return ret as ImportMetaEnv
+}
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), ['VITE_', 'APP_'])
   const ENV = useEnv(env)
-  console.log(env, ENV)
+
   return {
     envPrefix: ['VITE_', 'APP_'],
     base: ENV.VITE_BASE,
@@ -29,6 +46,10 @@ export default defineConfig(({ mode }) => {
           rewrite: (path) => path.replace(/^\/api\//, '')
         }
       }
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom'
     }
   }
 })
